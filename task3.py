54f1afd39a915790600fd68e40114578d5c05153
bala <balasingle2006@gmail.com>
2026-07-28 13:56:21 +0530

    Implement weight adjustment using gain factor
    Added gain factor to adjust weights before evaluation.

--- task3.py
+++ task3.py
@@ -3,10 +3,21 @@ weights = [100, 98, 94, 90, 101]
 
 threshold = 95
 
-for weight in weights:
-    print("Weight:", weight)
+# Gain value
+gain = 1.0
 
-    if weight < threshold:
+print("Gain:", gain)
+print("----------------")
+
+for weight in weights:
+
+    # Apply gain
+    adjusted_weight = weight * gain
+
+    print("Original Weight:", weight)
+    print("Adjusted Weight:", adjusted_weight)
+
+    if adjusted_weight < threshold:
         print("-> Reject Pack")
     else:
         print("-> Accept Pack")
